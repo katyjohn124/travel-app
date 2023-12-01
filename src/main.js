088051b32e134b8b2427274dc287b116f45f0694
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2023-12-01 17:22:18 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
 import store from './store'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
 //使用路由
 Vue.use(VueRouter)
+//解决移动端300ms延迟
+fastClick.attach(document.body)
 
 
 Vue.config.productionTip = false
---
+++ src/pages/Home/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        hello world
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+
+};
+</script>
+
+<style lang="scss" scoped></style>
