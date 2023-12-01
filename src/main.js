9c05e34f2131c2abd095c46f2e4374f34ff8b00d
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2023-12-01 15:33:51 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
+import store from './store'
+//使用路由
+Vue.use(VueRouter)
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue"
+import Vuex from "vuex"
+Vue.use(Vuex)
+
+const state = {
+
+};
+const mutations = {
+
+};
+const actions = {
+
+};
+const getters = {
+
+};
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters
+})
