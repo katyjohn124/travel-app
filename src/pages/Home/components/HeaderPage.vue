8f06aa66405910700f80c2cd65db56227f56ca23
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2023-12-02 15:56:45 +0800

    Add untracked files

---
+++ src/pages/Home/components/HeaderPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="header">
+        <div class="headerLeft"><span class="iconfont icon-fanhui"></span></div>
+        <div class="headerInput"><span class="iconfont icon-a-dangan_huaban1fuben5"></span>输入城市/景点/游玩主题</div>
+        <div class="headerRight">城市<span class="iconfont icon-diquxialajiantou"></span></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderPage',
+
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    align-items: center // 确保所有子项垂直居中
+    color: #fff
+    text-align: center
+    font-size: .32rem
+    justify-content: space-between // 添加这个属性来分散子元素
+    .headerLeft
+        width: .64rem
+        //float: left
+    .headerInput
+        height : .64rem
+        line-height: .64rem
+        border-radius: .1rem
+        flex: 1
+        margin-top: .12rem
+        margin-left: .1rem
+        padding-left: .1rem
+        margin-right: .1rem
+        background: #fff
+        color: #ccc
+        
+    .headerRight
+        width: .98rem
+        //float: right
+        display: flex
+        align-items: center // 如果里面有多个元素，确保它们垂直居中
+        //flex-direction: row 
+</style>
